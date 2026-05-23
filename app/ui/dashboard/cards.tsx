ea60c1134e16c9199d5f918d87216f3290890702
Orlando <orlandopinelo@hotmail.com>
2026-05-23 11:07:52 -0600

    Chapter 16 uploaded funtional 100%

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -6,6 +6,7 @@ import {
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData } from '@/app/lib/data';
+
 const iconMap = {
   collected: BanknotesIcon,
   customers: UserGroupIcon,
@@ -24,14 +25,14 @@ export default async function CardWrapper() {
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> 
+      /> */}
     </>
   );
 }
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,9 +1,11 @@
-'use client'
+'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
@@ -18,10 +20,9 @@ const links = [
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
-import Link from 'next/link';
 
 export default function NavLinks() {
-const pathname = usePathname();
+  const pathname = usePathname();
   return (
     <>
       {links.map((link) => {
@@ -30,13 +31,13 @@ const pathname = usePathname();
           <Link
             key={link.name}
             href={link.href}
-             className={clsx(
+            className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
                 'bg-sky-100 text-blue-600': pathname === link.href,
               },
             )}
-            >
+          >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </Link>
